824e4921479a5453b855ebd9f434a76adcebe948
karroot <gabbo97pop@hotmail.it>
2019-12-26 00:39:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,3 +20,31 @@ mongoose.connect("mongodb+srv://gabriele:qyun36zrbAZUYtv@myhealthcaredb-mpvmb.mo
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server running on port 3000");
 });
+
+
+app.post("/person", (request, response) => {
+    collection.insert(request.body, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result.result);
+    });
+});
+
+app.get("/people", (request, response) => {
+    collection.find({}).toArray((error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
+
+app.get("/person/:id", (request, response) => {
+    collection.findOne({ "_id": new ObjectId(request.params.id) }, (error, result) => {
+        if(error) {
+            return response.status(500).send(error);
+        }
+        response.send(result);
+    });
+});
